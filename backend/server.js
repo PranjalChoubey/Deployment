2d1900aca5807d918406a7a4b51b6b4317249afd
PranjalChoubey <103929097+PranjalChoubey@users.noreply.github.com>
2024-11-12 18:09:11 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,16 +7,26 @@ import userRouter from './routes/userRoute.js'
 import productRouter from './routes/productRoute.js'
 import cartRouter from './routes/cartRoute.js'
 import orderRouter from './routes/orderRoute.js'
+import couponRoutes from './routes/couponRoutes.js';
+import adminRoutes from './routes/adminRoutes.js';
+
+
 
 // App Config
 const app = express()
 const port = process.env.PORT || 4000
+const couponRoutes = require('./routes/couponRoutes');
+const adminRoutes = require('./routes/adminRoutes');
+
+
 connectDB()
 connectCloudinary()
 
 // middlewares
 app.use(express.json())
 app.use(cors())
+app.use('/api/coupons', couponRoutes);
+app.use('/api/admin', adminRoutes);
 
 // api endpoints
 app.use('/api/user',userRouter)
